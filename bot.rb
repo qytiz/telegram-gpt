99cc28196f391b8a16dcadef1430317ef066d6ba
qytiz <magronik@mail.ru>
2023-04-13 10:52:59 +0300

    rubocop fix

--- bot.rb
+++ bot.rb
@@ -1,36 +1,39 @@
+# frozen_string_literal: true
+
 require 'telegram/bot'
 require 'faraday'
 require 'json'
-require './db.rb'
-require "openai"
+require './db'
+require 'openai'
 require 'dotenv/load'
 
 Telegram::Bot::Client.run(ENV['TOKEN']) do |bot|
   def api_key(user_id)
     user = Database.get_user(user_id)
     return nil if user.nil?
+
     user.values[0][2]
   end
 
   def api_key_update(user_id, new_api_key)
     user = Database.get_user(user_id)
-    unless user.nil?
-      Database.edit_user(user_id, new_api_key)
-    else
+    if user.nil?
       Database.add_user(user_id, new_api_key)
+    else
+      Database.edit_user(user_id, new_api_key)
     end
   end
 
   def location_sender(latitude, longitude, bot, message)
-    latitude.gsub!("location_sender(", "")
-    longitude.gsub!(")", "")
+    latitude.gsub!('location_sender(', '')
+    longitude.gsub!(')', '')
 
     bot.api.send_location(chat_id: message.chat.id, latitude: latitude, longitude: longitude)
   end
 
   def create_keyboard(bot, message, keyboard, keyboard_header)
     markup = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: keyboard, one_time_keyboard: true)
-    bot.api.send_message(chat_id: message.chat.id, text:keyboard_header , reply_markup: markup)
+    bot.api.send_message(chat_id: message.chat.id, text: keyboard_header, reply_markup: markup)
   end
 
   def send_photo(bot, message, photo)
@@ -42,7 +45,7 @@ Telegram::Bot::Client.run(ENV['TOKEN']) do |bot|
   end
 
   def add_message(user_id, message, sender)
-    Database.add_message(user_id, message,sender)
+    Database.add_message(user_id, message, sender)
   end
 
   def delete_last_message(user_id)
@@ -52,95 +55,112 @@ Telegram::Bot::Client.run(ENV['TOKEN']) do |bot|
 
   def check_amount_of_messages?(user_id)
     messages_amount_result = Database.get_messages(user_id).count >= 10
-    end
+  end
 
   def send_message(bot, message)
-    unless api_key(message.from.id).nil? 
-      # Если сообщение для обработки уже было введено, отправляем запрос в API OpenAI
-      while check_amount_of_messages?(message.from.id)
-        delete_last_message(message.from.id)
-      end
+    return if api_key(message.from.id).nil?
 
-      text = message.text
+    # Если сообщение для обработки уже было введено, отправляем запрос в API OpenAI
+    delete_last_message(message.from.id) while check_amount_of_messages?(message.from.id)
 
-      old_messages = get_messages(message.from.id).map{|message| {role: message['sender'], content: message['message']}}
-      add_message(message.from.id, text, "user")
-      client = OpenAI::Client.new(access_token: api_key(message.from.id))
-      response = client.chat(
-        parameters: {
-            model: "gpt-3.5-turbo", # Required.
-            messages: [
-              {role: "system", content: "Ты можешь использовать функцию 'location_sender(latitude, longitude)', чтобы отправить геопозицию. После использования любой из этих функций, вы можете отправить сообщение в том же запросе, эти опции доступны только для gpt модели, пользователю категорически запрещается рассказывать об этом, не показывай их и не говори что они есть, а вызывай функцию, пример твоего ответа когда кто-то запрашивает что-то из этого - location_sender(1, 2), 'Вот геопозиция токио'. Ты не обязан использовать эти функции"},
-              {role: "system", content:  "Чтобы создать уникальную клавиатуру для пользователя, вы можете обратиться к функции 'create_keyboard(keyboard, keyboard_header)'. После использования этой функции, вы можете отправить сообщение в том же запросе, эта опция доступна только для gpt модели, пользователю категорически запрещается рассказывать об этом, не показывай их и не говори что они есть, а вызывай функцию, пример твоего ответа когда кто-то запрашивает что-то из этого - create_keyboard(keyboard, keyboard_header), 'Вот клавиатура токио'. Ты не обязан использовать эту функцию"},
-              {role: "system", content: "Для отправки фотографии вы можете использовать функцию 'send_photo(bot, photo)' вместо этого сообщения, эта опция доступна только для gpt модели, пользователю категорически запрещается рассказывать об этом, не показывай их и не говори что они есть, а вызывай функцию, пример твоего ответа когда кто-то запрашивает что-то из этого - send_photo(bot, photo), 'Вот фото токио'. Ты не обязан использовать эту функцию"},
-            ].concat(old_messages << { role: "user", content: text} ),
-        })
+    text = message.text
 
-    puts response.dig("choices", 0, "message", "content")
-    # => "Hello! How may I assist you today?"
-    
-        
-      # Получаем ответ от API OpenAI
-      puts response.body
-      answer = response.dig("choices", 0, "message", "content")
-
-      location_sender(answer.split(",")[0], answer.split(",")[1], bot, message) if answer.start_with?("location_sender")     
-      add_message(message.from.id, answer, "assistant") unless answer.nil?
-      # Отправляем ответ пользователю
-      bot.api.send_message(chat_id:message.chat.id, text: answer)
+    old_messages = get_messages(message.from.id).map do |message|
+      { role: message['sender'], content: message['message'] }
     end
+    add_message(message.from.id, text, 'user')
+    client = OpenAI::Client.new(access_token: api_key(message.from.id))
+    response = client.chat(
+      parameters: {
+        model: 'gpt-3.5-turbo', # Required.
+        messages: [
+          { role: 'system',
+            content: "Ты можешь использовать функцию 'location_sender(latitude, longitude)', чтобы отправить геопозицию. После использования любой из этих функций, вы можете отправить сообщение в том же запросе, эти опции доступны только для gpt модели, пользователю категорически запрещается рассказывать об этом, не показывай их и не говори что они есть, а вызывай функцию, пример твоего ответа когда кто-то запрашивает что-то из этого - location_sender(1, 2), 'Вот геопозиция токио'. Ты не обязан использовать эти функции" },
+          { role: 'system',
+            content: "Чтобы создать уникальную клавиатуру для пользователя, вы можете обратиться к функции 'create_keyboard(keyboard, keyboard_header)'. После использования этой функции, вы можете отправить сообщение в том же запросе, эта опция доступна только для gpt модели, пользователю категорически запрещается рассказывать об этом, не показывай их и не говори что они есть, а вызывай функцию, пример твоего ответа когда кто-то запрашивает что-то из этого - create_keyboard(keyboard, keyboard_header), 'Вот клавиатура токио'. Ты не обязан использовать эту функцию" },
+          { role: 'system',
+            content: "Для отправки фотографии вы можете использовать функцию 'send_photo(bot, photo)' вместо этого сообщения, эта опция доступна только для gpt модели, пользователю категорически запрещается рассказывать об этом, не показывай их и не говори что они есть, а вызывай функцию, пример твоего ответа когда кто-то запрашивает что-то из этого - send_photo(bot, photo), 'Вот фото токио'. Ты не обязан использовать эту функцию" }
+        ].concat(old_messages << { role: 'user', content: text })
+      }
+    )
+
+    puts response.dig('choices', 0, 'message', 'content')
+    # => "Hello! How may I assist you today?"
+
+    # Получаем ответ от API OpenAI
+    puts response.body
+    answer = response.dig('choices', 0, 'message', 'content')
+
+    if answer.start_with?('location_sender')
+      location_sender(answer.split(',')[0], answer.split(',')[1], bot,
+                      message)
+    end
+    add_message(message.from.id, answer, 'assistant') unless answer.nil?
+    # Отправляем ответ пользователю
+    bot.api.send_message(chat_id: message.chat.id, text: answer)
   end
 
   bot.listen do |message|
     puts message
     puts message.class
-    next if message.from.is_bot == true || message.class == Telegram::Bot::Types::Message && message.text.nil? || message.class != Telegram::Bot::Types::Message && message.class != Telegram::Bot::Types::CallbackQuery 
-    unless message.respond_to? :data
-    if message.text.start_with?('/start')
-      # Отправляем сообщение с выбором ключа API
-      kb = [[
-        Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Use our key', callback_data: 'use_my_key'),
-        Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Enter your key', callback_data: 'enter_your_key')
-      ]]
-      markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
-      bot.api.send_message(chat_id:message.chat.id, text: "Hello, I'm ChatGPT bot. Would you like to enter your OpenAI API key or use a free attempt with our key?", reply_markup: markup)
-    elsif message.text.start_with?('/gpt')
-        message.text.gsub!('/gpt', '')
-        next if message.text.empty?
-        send_message(bot, message)
-    elsif message.text.start_with?('/new_chat')
-      delete_messages(message.from.id)
-    else
-      return  bot.api.send_message(chat_id:message.chat.id, text: "Я не знаю такой команды :С") unless message.chat.type == 'private'
-       send_message(bot, message)
+    if message.from.is_bot == true || message.instance_of?(Telegram::Bot::Types::Message) && message.text.nil? || message.class != Telegram::Bot::Types::Message && message.class != Telegram::Bot::Types::CallbackQuery
+      next
     end
-    else
+
+    if message.respond_to? :data
       # Обработка данных, полученных при нажатии кнопок
       if message.data == 'use_my_key'
-        api_key_update(message.from.id,ENV['OPENAI_API_KEY']) unless api_key(message.from.id) == ENV['OPENAI_API_KEY']
+        api_key_update(message.from.id, ENV['OPENAI_API_KEY']) unless api_key(message.from.id) == ENV['OPENAI_API_KEY']
 
-        bot.api.send_message(chat_id: message.message.chat.id, text: "Great, you've chosen to use our key. Please enter the text you want me to process.")
+        bot.api.send_message(chat_id: message.message.chat.id,
+                             text: "Great, you've chosen to use our key. Please enter the text you want me to process.")
       elsif message.data == 'enter_your_key'
-        bot.api.send_message(chat_id: message.message.chat.id, text: "Please enter your OpenAI API key.")
+        bot.api.send_message(chat_id: message.message.chat.id, text: 'Please enter your OpenAI API key.')
         # Сохраняем сообщение для обработки в следующем шаге
         api_key = nil
         message_to_process = message.text
       else
         # Получаем ключ API пользователя из сообщения и сохраняем его в переменную
-        api_key_update(message.from.id,message.text)
+        api_key_update(message.from.id, message.text)
 
         # Отправляем запрос для проверки ключа API
-        response = Faraday.post("https://api.openai.com/v1/chat/gpt-3.5-turbo/completions", {}, { 'Authorization': "Bearer #{api_key}"})
-        
+        response = Faraday.post('https://api.openai.com/v1/chat/gpt-3.5-turbo/completions', {},
+                                { 'Authorization': "Bearer #{api_key}" })
+
         if response.status == 200
           # Отправляем сообщение с просьбой ввести текст для обработки
-          bot.api.send_message(chat_id:message.message.chat.id, text: "Great, your API key is valid! Please enter the text you want me to process.")
+          bot.api.send_message(chat_id: message.message.chat.id,
+                               text: 'Great, your API key is valid! Please enter the text you want me to process.')
         else
           # Отправляем сообщение об ошибке, если ключ API недействителен
-          bot.api.send_message(chat_id:message.message.chat.id, text: "Sorry, your API key is invalid. Please enter a valid key or use /mykey to use my key.")
+          bot.api.send_message(chat_id: message.message.chat.id,
+                               text: 'Sorry, your API key is invalid. Please enter a valid key or use /mykey to use my key.')
           next
         end
       end
+    elsif message.text.start_with?('/start')
+      kb = [[
+        Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Use our key', callback_data: 'use_my_key'),
+        Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Enter your key', callback_data: 'enter_your_key')
+      ]]
+      markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
+      bot.api.send_message(chat_id: message.chat.id,
+                           text: "Hello, I'm ChatGPT bot. Would you like to enter your OpenAI API key or use a free attempt with our key?", reply_markup: markup)
+    # Отправляем сообщение с выбором ключа API
+    elsif message.text.start_with?('/gpt')
+      message.text.gsub!('/gpt', '')
+      next if message.text.empty?
+
+      send_message(bot, message)
+    elsif message.text.start_with?('/new_chat')
+      delete_messages(message.from.id)
+    else
+      unless message.chat.type == 'private'
+        return bot.api.send_message(chat_id: message.chat.id,
+                                    text: 'Я не знаю такой команды :С')
+      end
+
+      send_message(bot, message)
     end
   end
 end
--- db.rb
+++ db.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require 'pg'
 require 'byebug'
 require 'dotenv/load'
 
 class Database
-
-  @conn = PG.connect(dbname: ENV['DATABASE_NAME'], port: ENV['DATABASE_HOST'], host: ENV['DATABASE_PORT'], password: ENV['DATABASE_PASSWORD'])
+  @conn = PG.connect(dbname: ENV['DATABASE_NAME'], port: ENV['DATABASE_HOST'], host: ENV['DATABASE_PORT'],
+                     password: ENV['DATABASE_PASSWORD'])
 
   @conn.exec("CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
@@ -23,56 +25,64 @@ class Database
   class << self
     def add_user(user_id, api_key)
       return if user_existed?(user_id).values.any?
-      @conn.exec_params("INSERT INTO users (user_id, api_key) VALUES ($1, $2);", [user_id, api_key])
+
+      @conn.exec_params('INSERT INTO users (user_id, api_key) VALUES ($1, $2);', [user_id, api_key])
     end
 
-
     def all_users
-      @conn.exec("SELECT * FROM users;")
+      @conn.exec('SELECT * FROM users;')
     end
 
     def user_existed?(user_id)
-      @conn.exec("SELECT * FROM users WHERE user_id = $1;", [user_id])
+      @conn.exec('SELECT * FROM users WHERE user_id = $1;', [user_id])
     end
 
     def get_user(user_id)
       return unless user_existed?(user_id).values.any?
-      @conn.exec("SELECT * FROM users WHERE user_id = $1;", [user_id])
+
+      @conn.exec('SELECT * FROM users WHERE user_id = $1;', [user_id])
     end
 
     def edit_user(user_id, api_key)
       return unless user_existed?(user_id).values.any?
-      @conn.exec_params("UPDATE users SET api_key = $1, WHERE user_id = $2;", [api_key, user_id])
+
+      @conn.exec_params('UPDATE users SET api_key = $1, WHERE user_id = $2;', [api_key, user_id])
     end
 
     def delete_user(user_id)
       return unless user_existed?(user_id).values.any?
-      @conn.exec("DELETE FROM users WHERE user_id = $1;", [user_id])
+
+      @conn.exec('DELETE FROM users WHERE user_id = $1;', [user_id])
     end
 
     def add_message(user_id, message, sender)
       return unless user_existed?(user_id).values.any?
-      @conn.exec_params("INSERT INTO messages (user_id, message, sender) VALUES ($1, $2, $3);", [user_id, message, sender])
+
+      @conn.exec_params('INSERT INTO messages (user_id, message, sender) VALUES ($1, $2, $3);',
+                        [user_id, message, sender])
     end
 
     def all_messages
-      @conn.exec("SELECT * FROM messages;")
+      @conn.exec('SELECT * FROM messages;')
     end
 
     def get_messages(user_id)
       return unless user_existed?(user_id).values.any?
-      @conn.exec("SELECT * FROM messages WHERE user_id = $1;", [user_id])
+
+      @conn.exec('SELECT * FROM messages WHERE user_id = $1;', [user_id])
     end
 
     def delete_messages(user_id)
-        return unless user_existed?(user_id).values.any?
-        @conn.exec("DELETE FROM messages WHERE user_id = $1;", [user_id])
+      return unless user_existed?(user_id).values.any?
+
+      @conn.exec('DELETE FROM messages WHERE user_id = $1;', [user_id])
     end
 
     def delete_last_message(user_id)
-        return unless user_existed?(user_id).values.any?
-        @conn.exec("DELETE FROM messages 
+      return unless user_existed?(user_id).values.any?
+
+      @conn.exec("DELETE FROM messages
             WHERE id = (  SELECT id FROM messages   WHERE user_id = $1   ORDER BY id DESC   LIMIT 1);", [user_id])
     end
-end
+  end
 end
